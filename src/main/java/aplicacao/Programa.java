919d15ce937f720ccf18d8fb0ade7336fc302a3b
Augusto Fernandes <augusto.fcosta1808@gmail.com>
2022-02-21 18:42:21 -0300

    utilizando o find

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -8,21 +8,14 @@ import dominio.Pessoa;
 
 public class Programa {
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
+		//find é uma função que busca um objeto pelo id
+		Pessoa p = em.find(Pessoa.class, 2);
 		
-		//persist é o que pega um objeto e salva no banco de dados
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		
-		em.getTransaction().commit();
+		System.out.println(p);
 		System.out.println("Pronto!");
 		
 		em.close();
